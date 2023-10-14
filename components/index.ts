6598f12ae891e34411dfe2dc81d44dc9007d1b40
Viraj Roy <116304823+vi-r-oy@users.noreply.github.com>
2023-10-14 08:40:34 +0530

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -3,7 +3,7 @@ import CustomButton from "./CustomButton";
 import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
-import ShowMore from "./ShowMore";
+
 import SearchBar from "./SearchBar";
 import Hero from "./Hero";
 
@@ -14,6 +14,6 @@ export {
   CustomFilter,
   Footer,
   NavBar,
-  ShowMore,
+ 
   SearchBar,
 };
